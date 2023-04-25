4a376ef4bdf325df91e5910f9cf55363b4a1855d
Luke Salazar <lucassy07@outlook.com>
2023-04-25 10:16:40 -0500

    First commit

---
+++ src/main/java/com/example/fintbackend/FintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fintbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintBackendApplication.class, args);
+    }
+
+}
