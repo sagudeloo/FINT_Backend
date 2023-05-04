7cc6f11fdb37896a7f025953e805c5bfb5d5a8ab
Jgrisal6 <grisalesjuanpablo18@gmail.com>
2023-05-03 19:09:38 -0500

    Tables/classes relationships included

---
+++ src/main/java/com/example/fintbackend/persistence/entity/Module.java
@@ -0,0 +1,50 @@
+package com.example.fintbackend.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "modules")
+public class Module {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "module_id")
+    private Integer idModule;
+
+    @Column(name = "module_title")
+    private String moduleTitle;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "module_id", insertable = false, updatable = false)
+    private UserModule userModule;
+
+    @OneToMany(mappedBy = "module")
+    private List<Subject> subject;
+
+    public Integer getIdModule() {
+        return idModule;
+    }
+
+    public void setIdModule(Integer idModule) {
+        this.idModule = idModule;
+    }
+
+    public String getModuleTitle() {
+        return moduleTitle;
+    }
+
+    public void setModuleTitle(String moduleTitle) {
+        this.moduleTitle = moduleTitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/example/fintbackend/persistence/entity/Subject.java
@@ -0,0 +1,51 @@
+package com.example.fintbackend.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "subjects")
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subject_id")
+    private Integer idSubject;
+
+    @Column(name = "subject_title")
+    private String subjectTitle;
+
+    @Column(name = "subject_progress")
+    private Integer subjectProgress;
+
+    @ManyToOne
+    @JoinColumn(name = "module_id", insertable = false, updatable = false)
+    private Module module;
+
+    @OneToMany(mappedBy = "subject")
+    private List<Content> content;
+
+    public Integer getIdSubject() {
+        return idSubject;
+    }
+
+    public void setIdSubject(Integer idSubject) {
+        this.idSubject = idSubject;
+    }
+
+    public String getSubjectTitle() {
+        return subjectTitle;
+    }
+
+    public void setSubjectTitle(String subjectTitle) {
+        this.subjectTitle = subjectTitle;
+    }
+
+    public Integer getSubjectProgress() {
+        return subjectProgress;
+    }
+
+    public void setSubjectProgress(Integer subjectProgress) {
+        this.subjectProgress = subjectProgress;
+    }
+}
