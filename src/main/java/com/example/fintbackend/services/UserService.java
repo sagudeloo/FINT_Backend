01547211d9364733734ebcc263bf57696340c682
Luke Salazar <lucassy07@outlook.com>
2023-05-04 23:29:38 -0500

    Feature: UserServices

--- src/main/java/com/example/fintbackend/services/UserService.java
+++ src/main/java/com/example/fintbackend/services/UserService.java
@@ -6,6 +6,7 @@ import com.example.fintbackend.repositories.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
@@ -16,7 +17,7 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public ResponseEntity readAll(){
+    public ResponseEntity getAll(){
         var a = userRepository.findAll();
         return new ResponseEntity(a, HttpStatus.OK);
     }
@@ -29,5 +30,31 @@ public class UserService {
         return new ResponseEntity("Create new user", HttpStatus.CREATED);
     }
 
+    //TODO: @ApiResponses not working... Check dependencies
+
+    public ResponseEntity getById(@PathVariable Long id){
+        var a = userRepository.findById(id);
+        if(a.isEmpty()){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(a.get(), HttpStatus.OK);
+    }
+
     //TODO: Currently mixing content of UserControllersSQL + services in SWMDBJWT
+
+    public ResponseEntity updateById(@RequestBody UserDto user, @PathVariable Long id){
+        var a = userRepository.existsById(id);
+        if(a){
+            var b = userRepository.findById(id).get();
+            b = b.withName(user.getName()).withPassword(user.getPassword()).withEmail(user.getEmail());
+            userRepository.save(b);
+            return new ResponseEntity(b, HttpStatus.OK);
+        }
+        return new ResponseEntity("El usuario buscado no existe", HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity deleteById(@PathVariable Long id){
+        userRepository.deleteById(id);
+        return new ResponseEntity("El usuario ha sido borrado", HttpStatus.NO_CONTENT);
+    }
 }
